ae418c17f1882b565b03bc6cb09317c3df5dab76
logan <logan.saliga@gmail.com>
2023-11-09 12:26:49 -0500

    working tictactoe, initial

---
+++ src/components/Square.js
@@ -0,0 +1,10 @@
+export function Square({ value, onSquareClick }) {
+    return (
+        <button 
+            className="square" 
+            onClick={onSquareClick}
+        >
+            {value}
+        </button>
+    );
+}
